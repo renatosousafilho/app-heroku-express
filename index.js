53697b1f175e2a38e922a3f61f82762e62c1bc03
renato <renato@MacBook-Pro-de-renato.local>
2022-02-09 17:49:15 -0300

    v1 add route users

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get('/ping', (req, res) => {
   res.status(200).send('pong!')
 });
 
+app.get('/users', (req, res) => {
+  res.json([
+    { id: 1, name: 'Renato' },
+    { id: 2, name: 'Kissyla' },
+    { id: 3, name: 'Italo' }
+  ]);
+})
+
 
 const PORT = process.env.PORT || 3000;
 
