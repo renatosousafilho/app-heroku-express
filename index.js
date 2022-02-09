0cb77d03740de8ab4b1de2d28391b7cf1a8da93d
renato <renato@MacBook-Pro-de-renato.local>
2022-02-09 17:37:46 -0300

    Add SERVER_ENV in index.js

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 
+const SERVER_ENV = process.env.SERVER_ENV;
+
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, heroku!')
+  res.status(200).send(`Hello, heroku! Running in ${SERVER_ENV} enviroment!`)
 });
 
 app.get('/ping', (req, res) => {
   res.status(200).send('pong!')
 });
+
 
 const PORT = process.env.PORT || 3000;
 
