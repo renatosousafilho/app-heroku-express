7d73e4744dc1c18d2353d730d26e3a9b33aa84f4
renato <renato@MacBook-Pro-de-renato.local>
2022-02-09 17:06:03 -0300

    Add route /

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, heroku!')
+});
+
 app.get('/ping', (req, res) => {
   res.status(200).send('pong!')
-})
+});
 
 const PORT = process.env.PORT || 3000;
 
